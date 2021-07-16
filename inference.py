497fb7555949aea7194f20098e2e93f784ea1a70
msd97 <mariosilvestre@hotmail.es>
2021-07-16 18:04:28 -0500

    Solved some errors

--- inference.py
+++ inference.py
@@ -80,7 +80,7 @@ while(True):
     # the 'q' button is set as the
     # quitting button you may use any
     # desired button of your choice
-    if cv2.waitKey(80) & 0xFF == ord('q'):
+    if cv2.waitKey(60) & 0xFF == ord('q'):
         break
   
 # After the loop release the cap object
